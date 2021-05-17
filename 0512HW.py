090217518c20482d944f6536cf52b15b3491fe6f
wwhales11 <11suhwan@gmail.com>
2021-05-17 09:45:16 +0900

    update

--- 0512HW.py
+++ 0512HW.py
@@ -1,9 +1,9 @@
 # 다이아몬드 만들기
 
-row = int(input("size of Diamond: "))
+line = int(input("size of Diamond: "))
 
-for num1 in range(1, row * 2, 2):
-    print((" " * ((row * 2 - 1 - num1) // 2)) + ("*" * num1))
+for row1 in range(1, line * 2, 2):
+    print((" " * ((line * 2 - 1 - row1) // 2)) + ("*" * row1))
 
-for num2 in range(row * 2 - 3, 0, -2):
-    print((" " * ((row * 2 - 1 - num2) // 2)) + "*" * num2)
+for row2 in range(line * 2 - 3, 0, -2):
+    print((" " * ((line * 2 - 1 - row2) // 2)) + "*" * row2)
